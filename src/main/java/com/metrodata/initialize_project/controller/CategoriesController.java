78944dc25642901214b9f3a401eb16918f15dd30
muhammadnazlinashri <112940962+muhammadnazlinashri@users.noreply.github.com>
2024-06-12 23:11:15 +0700

    tb_m_categories
    membuat CRUD tb_m_categories

---
+++ src/main/java/com/metrodata/initialize_project/controller/CategoriesController.java
@@ -0,0 +1,53 @@
+package com.metrodata.initialize_project.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.metrodata.initialize_project.entity.Article;
+import com.metrodata.initialize_project.entity.Categories;
+import com.metrodata.initialize_project.service.CategoriesService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ArticleCategories")
+public class CategoriesController {
+
+    private final CategoriesService categoriesService;
+    
+     @GetMapping
+    public List<Categories> getAll() {
+        return categoriesService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Categories getByIdCategories(@PathVariable Long id) {
+        return categoriesService.getByIdCategories(id);
+
+    }
+
+    @PostMapping("/create")
+    public Categories create(@RequestBody Categories categories) {
+        return categoriesService.createCategoriesArticle(categories);
+    }
+
+    @PutMapping("/{id}")
+    public Categories update(@RequestBody Categories categories, @PathVariable Long id) {
+        return categoriesService.updateCategories(categories, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Categories deleteByIdCategories(@PathVariable Long id) {
+        return categoriesService.deleteByIdCategories(id);
+    }
+    
+}
--- src/main/java/com/metrodata/initialize_project/service/CategoriesService.java
+++ src/main/java/com/metrodata/initialize_project/service/CategoriesService.java
@@ -1,9 +1,49 @@
 package com.metrodata.initialize_project.service;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.metrodata.initialize_project.entity.Article;
+import com.metrodata.initialize_project.entity.Categories;
+import com.metrodata.initialize_project.repository.ArticleRepository;
+import com.metrodata.initialize_project.repository.CategoriesRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class CategoriesService {
-    
-    
+
+    private final CategoriesRepository categoriesRepository;
+
+    public List<Categories> getAll() {
+        return categoriesRepository.findAll();
+    }
+
+    public Categories getByIdCategories(Long id) {
+        return categoriesRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "categories not found"));
+    }
+
+    public Categories createCategoriesArticle(Categories categories) {
+        categories.setName(categories.getName());
+        return categoriesRepository.save(categories);
+    }
+
+    public Categories updateCategories(Categories categories, Long id) {
+        getByIdCategories(id);
+        categories.setId(id);
+        return categoriesRepository.save(categories);
+    }
+
+    public Categories deleteByIdCategories(Long id) {
+        Categories categories = getByIdCategories(id);
+        categoriesRepository.delete(categories);
+        
+        return categories;
+    }
+
 }
