8b4dc48e6ea2b079194d12a3b9a063c5d6c76582
ibnukarimismail <ibnukais20@gmail.com>
2024-06-12 09:50:45 +0700

    initialize database connection

--- src/main/java/com/metrodata/initialize_project/InitializeProjectApplication.java
+++ src/main/java/com/metrodata/initialize_project/InitializeProjectApplication.java
@@ -8,6 +8,9 @@ public class InitializeProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitializeProjectApplication.class, args);
+		System.out.println("=================");
+        System.out.println("Server is running");
+        System.out.println("=================");
 	}
 
 }
